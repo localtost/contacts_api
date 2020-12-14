0e05992ac63057e0b2c803bd3d3989440c5de786
Kumskov-dev <Artemka19021991>
2020-12-14 14:49:22 +0200

    added connection to mongoDB

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express, { request } from 'express';
 import users from './routes/users';
 import auth from './routes/auth';
 import contacts from './routes/contacts';
+import connectDB from "./config/db";
 
 const server = express();
 
@@ -12,5 +13,5 @@ server.use('/api/users',users);
 
 const PORT = process.env.PORT || 5000;
 
-
+connectDB().then(()=>console.log('Db is connected'));
 server.listen(PORT,()=>console.log(`Server start om server on port ${PORT}`))

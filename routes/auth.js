3c5e88a3611c60dda17341ae3054ac53adfb17e1
Kumskov-dev <Artemka19021991>
2020-12-14 21:11:58 +0200

    created user model

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,13 @@
 import express from "express";
+import {
+  check,
+  validationResult
+} from "express-validator";
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import config from 'config';
+import User from '../model/User';
+
 
 const auth = express.Router();
 
@@ -10,10 +19,39 @@ auth.get("/", (req, res) => {
 });
 
 //@route    POST api/auth
-//@desc     Auth  user & get token 
+//@desc     Auth  user & get token
 //@@access  Public
-auth.post("/", (req, res) => {
-  res.send("Auth  user & get token ");
+auth.post("/", [
+    check('email','Email is required').isEmail(),
+    check('password','Password is required' ).not().isEmpty()
+], async (req, res) => {
+  const error = validationResult(req);
+  if (!error.isEmpty()) {
+    return res.status(400).json({errors: error.array()})
+  }
+  const {email,password} = req.body;
+  try {
+    let user = await User.findOne({email})
+    if(!user) {
+      return  res.status(400).json({msg:"Invalid Credential"})
+    }
+    const isMatch = await bcrypt.compare(password,user.password)
+    if(!isMatch) {
+      return  res.status(400).json({msg:"Invalid Credential"})
+    }
+    jwt.sign({
+      user: {
+        user : user.id
+      }
+    },config.get('jwtSecret'),{
+      expiresIn: 360000
+    },(err,token)=>{
+      if(err) throw err;
+      res.json({token})
+    })
+  }catch (error){
+
+  }
 });
 
 export default auth;
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,54 @@
 import express from "express";
+import User from '../model/User';
+import {
+    check,
+    validationResult
+} from "express-validator";
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import config from 'config'
 
 const users = express.Router();
 //@route    POST api/users
 //@desc     Get logged in user
 //@@access  Public
-users.post("/", (req, res) => {
-  res.send("Auth");
+users.post("/", [
+    check('name', 'Name is Required').not().isEmpty(),
+    check('email', 'Please include valid email').isEmail(),
+    check('password', "Please enter password").isLength({
+        min: 6
+    })
+], async (req, res) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+        return res.status(400).json({errors: error.array()})
+    }
+    const {name, email, password} = req.body;
+    try {
+        let user = await User.findOne({email})
+        if (user) {
+            return res.status(400).json({msg:'User already exist'})
+        }
+        const salt = await bcrypt.genSalt(10);
+        user = new User({
+            email,
+            name,
+            password: await bcrypt.hash(password, salt)
+        })
+        await user.save();
+        jwt.sign({
+          user: {
+            user : user.id
+          }
+        },config.get('jwtSecret'),{
+          expiresIn: 360000
+        },(err,token)=>{
+          if(err) throw err;
+          res.json({token})
+        })
+    } catch (e) {
+        res.status(500)
+    }
 });
 
 export default users;
